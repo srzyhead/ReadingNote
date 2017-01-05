ca63ed8e576ca90c669bb20a7e592e9df33110e6
srzyhead <srzyhead@yeah.net>
2017-01-05 18:18:12 +0800

    看到第六章了

--- TheLittleSchemer/Chapter4.rkt
+++ TheLittleSchemer/Chapter4.rkt
@@ -3,12 +3,12 @@
 (define (add1 n)
   (+ n 1))
 
-(eq? (add1 67) 68)
+(equal? (add1 67) 68)
 
 (define (sub1 n)
   (- n 1))
 
-(eq? (sub1 0) -1)
+(equal? (sub1 0) -1)
 
 (zero? 0)
 
@@ -21,8 +21,8 @@
     )
   )
 
-(eq? (o+ 1 2) 3)
-(eq? (o+ -1 -2) -3)
+(equal? (o+ 1 2) 3)
+(equal? (o+ -1 -2) -3)
 
 ; cons builds lists and addl builds numbers.
 
@@ -34,8 +34,8 @@
     )
   )
 
-(eq? (o- 1 2) -1)
-(eq? (o- -1 -2) 1)
+(equal? (o- 1 2) -1)
+(equal? (o- -1 -2) 1)
 
 (define (tup? lst)
   (cond
@@ -45,10 +45,10 @@
     )
   )
 
-(eq? (tup? `(8 55 5 555)) #t)
-(eq? (tup? `(1 2 8 apple 4 3)) #f)
-(eq? (tup? `(3 (7 4) 13 9)) #f)
-(eq? (tup? `()) #t)
+(eq? (tup? '(8 55 5 555)) #t)
+(eq? (tup? '(1 2 8 apple 4 3)) #f)
+(eq? (tup? '(3 (7 4) 13 9)) #f)
+(eq? (tup? '()) #t)
 
 (define (addtup lst)
   (define (addtup-temp lst1)
@@ -61,9 +61,9 @@
       #f)
   )
 
-(eq? (addtup `(8 55 5 555)) 623)
-(eq? (addtup `()) 0)
-(eq? (addtup `(3 (7 4) 13 9)) #f)
+(equal? (addtup '(8 55 5 555)) 623)
+(equal? (addtup '()) 0)
+(equal? (addtup '(3 (7 4) 13 9)) #f)
 
 #|
 The First Commandment
@@ -74,6 +74,26 @@ When recurring on a number, n, ask two questions about
 it: (zero? n) and else.
 |#
 
+
+#|
+The Fourth Commandment
+(first revision)
+Always change at least one argument while recurring. It
+must be changed to be closer to termination. The changing
+argument must be tested in the termination condition:
+when using cdr, test termination w ith null? and
+when using s u b l, test termination with zero?.
+|#
+
+(define (o* n m)
+  (cond
+    ((zero? m) 0)
+    (else (+ n (o* n (sub1 m))))
+    )
+  )
+
+(equal? (o* 123 3) 369)
+
 #|
 The Fifth Commandment
 When building a value with + , always use 0 for the value of the terminating line, for adding 0 does not change the value of an addition.
@@ -81,17 +101,134 @@ When building a value with x , always use 1 for the value of the terminating lin
 When building a value with cons ,always consider () for the value of the terminating line.
 |#
 
+(define (tup+ tup1 tup2)
+  (cond
+    ((null? tup1) tup2)
+    ((null? tup2) tup1)
+    (else (cons
+           (+ (car tup1) (car tup2))
+           (tup+ (cdr tup1) (cdr tup2))
+           ))
+    )
+  )
 
 
+(equal? (tup+ '(2 3) '(4 6)) '(6 9))
+(equal? (tup+ '(2 3 8 1) '(4 6)) '(6 9 8 1))
+
+(define (o> m n)
+  (cond
+    ((zero? m) #f)
+    ((zero? n) #t)
+    (else (o> (sub1 m) (sub1 n)))
+    )
+  )
+
+(equal? (o> 1 2) #f)
+(equal? (o> 12345 2) #t)
+(equal? (o> 2 2) #f)
+
+(define (o< m n)
+  (cond
+    ((zero? m) #t)
+    ((zero? n) #f)
+    (else (o< (sub1 m) (sub1 n)))
+    )
+  )
+
+(equal? (o< 1 2) #t)
+(equal? (o< 12345 2) #f)
+(equal? (o< 2 2) #t)
+
+(define (o= m n)
+  (cond
+    ((and (zero? m) (zero? n)) #t)
+    ((zero? n) #f)
+    ((zero? m) #f)
+    (else (o= (sub1 m) (sub1 n)))
+    )
+  )
+
+(equal? (o= 1 2) #f)
+(equal? (o= 12345 2) #f)
+(equal? (o= 2 2) #t)
+
+(define (o-expt m n)
+  (cond
+    ((zero? n) 1)
+    (else (* m (o-expt m (sub1 n))))
+    )
+  )
+
+(equal? (o-expt 2 2) 4)
+(equal? (o-expt 2 3) 8)
+
+(define (length lat)
+  (cond
+    ((null? lat) 0)
+    (else (+ 1 (length (cdr lat))))
+    )
+  )
+
+(equal? (length '(1 2 3)) 3)
+(equal? (length '()) 0)
+
+(define (pick n lat)
+  (cond
+    ((zero? (sub1 n)) (car lat))
+    (else (pick (sub1 n) (cdr lat)))
+    )
+  )
+
+(equal? (pick 1 '(1 2 3)) 1)
+
+(define (rempick n lat)
+  (cond
+    ((zero? (sub1 n)) (cdr lat))
+    (else (cons (car lat) (rempick (sub1 n) (cdr lat))))
+    )
+  )
+
+(equal? (rempick 1 '(1 2 3)) '(2 3))
+
+(define (no-nums lat)
+  (cond
+    ((null? lat) '())
+    ((number? (car lat)) (no-nums (cdr lat)) )
+    (else (cons (car lat) (no-nums (cdr lat))) )
+    )
+  )
+
+(equal? (no-nums '(1 asdf 2 3)) '(asdf))
 
 
+(define (all-nums lat)
+  (cond
+    ((null? lat) '())
+    ((not (number? (car lat))) (all-nums (cdr lat)) )
+    (else (cons (car lat) (all-nums (cdr lat))) )
+    )
+  )
 
+(equal? (all-nums '(1 asdf 2 3)) '(1 2 3))
 
+(define (occur a lat)
+  (cond
+    ((null? lat) 0)
+    ((eq? (car lat) a) (+ 1 (occur a (cdr lat))) )
+    (else (occur a (cdr lat)) )
+    )
+  )
 
+(equal? (occur 1 '(1 asdf 2 3 1)) 2)
 
+(define (one? n) (= n 1))
 
+(define (rempick2 n lat)
+  (cond
+    ((one? n) (cdr lat))
+    (else (cons (car lat) (rempick2 (sub1 n) (cdr lat))))
+    )
+  )
 
-
-
-
-
+(equal? (rempick2 1 '(1 2 3)) '(2 3))
---
+++ TheLittleSchemer/Chapter5.rkt
@@ -0,0 +1,220 @@
+#lang racket
+
+(require "Common.rkt")
+
+(define (rember* a l)
+  (cond
+    ((null? l) '())
+    ((and (list? l) (equal? a (car l))) (rember* a (cdr l)) )
+    ((list? l) (cons (rember* a (car l)) (rember* a (cdr l))) )
+    (else l)
+    )
+  )
+
+(equal? (rember* 'cup '((coffee) cup ((tea) cup) (and (hick)) cup)) '((coffee) ((tea)) (and (hick))))
+(equal? (rember* 'sauce '(((tomato sauce)) ((bean) sauce) (and ((flying)) sauce))) '(((tomato)) ((bean)) (and ((flying)))))
+
+(define (insertR* new old l)
+  (cond
+    ((null? l) '())
+    ((and (list? l) (equal? old (car l)))
+     (cons old (cons new (insertR* new old (cdr l)))) )
+    ((list? l)
+     (cons (insertR* new old (car l)) (insertR* new old (cdr l))) )
+    (else l)
+    )
+  )
+
+(equal?
+ (insertR*
+  'roast
+  'chuck
+  '((how much (wood)) could ((a (wood) chuck)) (((chuck)))
+                      (if (a) ((wood chuck))) could chuck wood))
+ '((how much (wood)) could ((a (wood) chuck roast)) (((chuck roast)))
+                     (if (a) ((wood chuck roast))) could chuck roast wood)
+ )
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                                                            ;
+; The first commandment (final version)                                      ;
+;                                                                            ;
+; When recurring on a list of atoms, lat, ask two questions about it:        ;
+; (null? lat) and else.                                                      ;
+; When recurring on a number, n, ask two questions about it: (zero? n) and   ;
+; else.                                                                      ;
+; When recurring on a list of S-expressions, l, ask three questions about    ;
+; it: (null? l), (atom? (car l)), and else.                                  ;
+;                                                                            ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                                                            ;
+; Why all *-functions similar?                                               ;
+;                                                                            ;
+; Because all *-functions work on lists that are either                      ;
+; - empty,                                                                   ;
+; - an atom consed onto a list, or                                           ;
+; - a list consed onto a list.                                               ;
+;                                                                            ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                                                            ;
+; The fourth commandment (final version)                                     ;
+;                                                                            ;
+; Always change at least one argument while recurring. When recurring on a   ;
+; list of atoms, lat, use (cdr l). When recurring on a number, n, use        ;
+; (sub1 n). And when recurring on a list of S-expressions, l, use (car l)    ;
+; and (cdr l) if neither (null? l) nor (atom? (car l)) are true.             ;
+;                                                                            ;
+; It must be changed to be closer to termination. The changing argument must ;
+; be tested in the termination condition:                                    ;
+; * when using cdr, test the termination with null? and                      ;
+; * when using sub1, test termination with zero?.                            ;
+;                                                                            ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+(define (occur* a l)
+  (cond
+    ((null? l) 0)
+    ((atom? l) (if (eq? a l) 1 0))
+    (else (+ (occur* a (car l)) (occur* a (cdr l))))
+    )
+  )
+
+(equal?
+ (occur*
+  'banana
+  '((banana)
+    (split ((((banana ice)))
+            (cream (banana))
+            sherbet))
+    (banana)
+    (bread)
+    (banana brandy)))
+ 5)
+
+
+; The subst* function substitutes all olds for news in l
+;
+(define (subst* new old l)
+  (cond
+    ((atom? l) (if (eq? old l) new l) )
+    ((null? l) `())
+    (else (cons (subst* new old (car l)) (subst* new old (cdr l))) )
+    )
+  )
+
+; Example of subst*
+;
+(equal? (subst*
+         'orange
+         'banana
+         '((banana)
+           (split ((((banana ice)))
+                   (cream (banana))
+                   sherbet))
+           (banana)
+           (bread)
+           (banana brandy)))
+        '((orange) (split ((((orange ice))) (cream (orange)) sherbet)) (orange) (bread) (orange brandy))
+        )
+
+; The insertL* function insers new to the left of all olds in l
+;
+(define (insertL* new old l)
+  (cond
+    ((null? l) `())
+    ((atom? (car l))
+     (if (eq? old (car l))
+         (cons new (cons old (insertL* new old (cdr l))))
+         (cons (car l) (insertL* new old (cdr l)))
+         ))
+    (else
+     (cons (insertL* new old (car l)) (insertL* new old (cdr l))))
+    )
+  )
+
+; Example of insertL*
+;
+(equal?
+ (insertL*
+  'pecker
+  'chuck
+  '((how much (wood)) could ((a (wood) chuck)) (((chuck)))
+                      (if (a) ((wood chuck))) could chuck wood))
+ '((how much (wood))
+   could
+   ((a (wood) pecker chuck))
+   (((pecker chuck)))
+   (if (a) ((wood pecker chuck)))
+   could pecker chuck wood)
+ )
+
+; The member* function determines if element is in a list l of s-exps
+;
+(define (member* a l)
+  (cond
+    ((null? l) #f)
+    ((atom? l) (if (eq? l a) #t #f))
+    (else
+     (or (member* a (car l)) (member* a (cdr l))))
+    )
+  )
+
+; Example of member*
+;
+(equal?
+ (member*
+  'chips
+  '((potato) (chips ((with) fish) (chips))))
+ #t)
+
+; The leftmost function finds the leftmost atom in a non-empty list
+; of S-expressions that doesn't contain the empty list
+;
+(define (leftmost l)
+  (cond
+    ((null? l) #f)
+    ((atom? l) l)
+    (else (leftmost (car l)))
+    )
+  )
+
+; Examples of leftmost
+;
+(equal?
+ (leftmost '((potato) (chips ((with) fish) (chips))))
+ 'potato)
+(equal?
+ (leftmost '(((hot) (tuna (and))) cheese))
+ 'hot)
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;                                                                            ;
+; The sixth commandment                                                      ;
+;                                                                            ;
+; Simplify only after the function is correct.                               ;
+;                                                                            ;
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+
+
+; rember simplified, it now also works on s-expressions, not just atoms
+;
+(define (rember s l)
+  (cond
+    ((null? l) '())
+    ((equal? (car l) s) (cdr l))
+    (else (cons (car l) (rember s (cdr l))))))
+
+; Example of rember
+;
+(equal?
+ (rember
+  '(foo (bar (baz)))
+  '(apples (foo (bar (baz))) oranges))
+ '(apples oranges)
+ )
+
